806a630b449a6778b1352f11f8c764febcdfb0b4
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-09-26 12:43:40 +0530

    Add files via upload

---
+++ multiply.java
@@ -0,0 +1,12 @@
+
+public class multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a=5;
+        int b=10;
+        int mul=a*b;
+        System.out.println(mul);
+	}
+
+}
