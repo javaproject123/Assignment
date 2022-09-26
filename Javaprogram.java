3f61e5f197b6ca8eeffb85a95d1030c297aa6352
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-09-26 12:04:38 +0530

    Add files via upload

---
+++ Javaprogram.java
@@ -0,0 +1,13 @@
+
+public class Javaprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       float first = 1.5f;
+       float second = 2.0f;
+       float product = first * second;
+       System.out.println("The product is:" + product);
+       
+	}
+
+}
