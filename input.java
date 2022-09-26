9eb7b63df63f37b426d4390b021556b5685f878b
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-09-26 12:50:59 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Scanner sc = new Scanner(System.in);
+     System.out.println("put a num");
+     int a= sc.nextInt();
+     System.out.println("input a num :" +a);
+	}
+
+}
